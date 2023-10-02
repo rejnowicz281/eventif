ede8039bd8790d0ab7f9218b158b8af2921d2629
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-02 23:07:59 +0200

    Make created membership either accepted or not based on if event is private

--- app/controllers/event_memberships_controller.rb
+++ app/controllers/event_memberships_controller.rb
@@ -2,13 +2,17 @@ class EventMembershipsController < ApplicationController
     before_action :set_event, only: [:create, :destroy]
 
     def create
-        @membership = EventMembership.create(user: current_user, event: @event)
+        if @event.private
+            @membership = EventMembership.create(user: current_user, event: @event)
+        else
+            @membership = EventMembership.create(user: current_user, event: @event, accepted: true)
+        end
 
         redirect_to @event
     end
 
     def destroy
-        @membership = EventMembership.find_by(user: @event)
+        @membership = EventMembership.find_by(event: @event)
 
         @membership.destroy
 
