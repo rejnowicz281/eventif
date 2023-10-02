7fc2c45156267a8e1f9e95da8f018dbb0d476c95
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-02 23:49:49 +0200

    Make seperate methods for event's accepted and non accepted members

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,21 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     
     has_many :event_memberships, dependent: :destroy
-    has_many :members, source: :user, through: :event_memberships
 
     validates_presence_of :name
     validates_presence_of :start_date, message: "must be set"
+
+    def accepted_members
+        event_memberships = self.event_memberships.where(accepted: true)
+        arr = []
+        event_memberships.each { |event_membership| arr.append(event_membership.user) } if !event_memberships.empty?
+        arr
+    end
+
+    def non_accepted_members
+        event_memberships = self.event_memberships.where(accepted: false)
+        arr = []
+        event_memberships.each { |event_membership| arr.append(event_membership.user) } if !event_memberships.empty?
+        arr
+      end
 end
