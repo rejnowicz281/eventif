889ee9b3ae74a6160542f9e0bd2ad3c840a4c209
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-02 22:45:39 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user = User.first
-Event.create(creator_id: user.id, name: "Event 1", description: "Funi event", start_date: Date.new(2023,1,1))
-Event.create(creator_id: user.id, name: "Event 2", start_date: Date.new(2023,1,2))
+user1 = User.create(email: "1@gmail.com", first_name: "User", last_name: "One", password: "123", password_confirmation: "123")
+user2 = User.create(email: "2@gmail.com", first_name: "User", last_name: "Two", password: "123", password_confirmation: "123")
+Event.create(creator: user1, name: "Event 1", description: "Funi event", start_date: Date.new(2023,1,1))
+Event.create(creator: user2, name: "Event 2", start_date: Date.new(2023,1,2))
