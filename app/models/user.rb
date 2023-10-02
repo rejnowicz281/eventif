c05c7c2fc66d02f7c051462158a6365fcbc4b81a
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-02 23:10:17 +0200

    Make seperate methods to get user's joined events and membership-not-accepted events

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,25 @@ class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: "creator_id", dependent: :destroy
   
   has_many :event_memberships, dependent: :destroy
-  has_many :joined_events, source: :event, through: :event_memberships
 
   validates :first_name, presence: true
   validates :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
-  end 
+  end
+
+  def joined_events
+    event_memberships = self.event_memberships.where(accepted: true)
+    arr = []
+    event_memberships.each { |event_membership| arr.append(event_membership.event) } if !event_memberships.empty?
+    arr
+  end
+
+  def pending_membership_events
+    event_memberships = self.event_memberships.where(accepted: false)
+    arr = []
+    event_memberships.each { |event_membership| arr.append(event_membership.event) } if !event_memberships.empty?
+    arr
+  end
 end
