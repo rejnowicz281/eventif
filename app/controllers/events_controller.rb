83eed1924c55664cb043ab618919494ed87d5790
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-02 23:13:02 +0200

    Events controller fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,11 +11,11 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = current_user.events.build
+    @event = current_user.created_events.build
   end
 
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to @event
